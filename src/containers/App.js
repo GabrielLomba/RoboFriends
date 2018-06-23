c9aa089c81570b1e496b80226f3fd24594f86fca
Gabriel Lomba <gabriellomba@hotmail.com>
2018-06-22 21:28:43 -0300

    Organizar componentes e refatorar App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -17,28 +18,29 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(
-            robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        const { robots, searchField } = this.state;
+        const filteredRobots = robots.filter(
+            robot => robot.name.toLowerCase().includes(searchField.toLowerCase())
         );
-        if(this.state.robots.length === 0) {
-            return (
-                <h1 className='tc'>Loading</h1>
-            );
-        } else {
+        if (robots.length) {
             return (
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <CardList robots={filteredRobots} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
                 </div>
             );
+        } else {
+            return <h1 className='tc'>Loading</h1>;
         }
     }
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots : users }));
+            .then(users => this.setState({ robots: users }));
     }
 }
 
