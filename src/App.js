58f2a52669b19e508b129bb2ad97466fcccee6a8
Gabriel Lomba <gabriellomba@hotmail.com>
2018-06-22 21:05:51 -0300

    Receber dados dos robôs através de uma request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         };
     }
@@ -20,14 +19,26 @@ class App extends Component {
     render() {
         const filteredRobots = this.state.robots.filter(
             robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-        )
-        return (
-            <div className='tc'>
-                <h1 className='f2'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
         );
+        if(this.state.robots.length === 0) {
+            return (
+                <h1 className='tc'>Loading</h1>
+            );
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f2'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots : users }));
     }
 }
 
