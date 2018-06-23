352e269cec26e3898fca823efd152ab0f10ee74b
Gabriel Lomba <gabriellomba@hotmail.com>
2018-06-22 23:34:34 -0300

    Usar Redux para guardar estado da pesquisa

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => ({
+    searchField: state.searchField
+});
+
+const mapDispatchToProps = dispatch => ({
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+});
+
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: [],
-            searchField: ''
+            robots: []
         };
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value });
-    }
-
     render() {
-        const { robots, searchField } = this.state;
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = robots.filter(
             robot => robot.name.toLowerCase().includes(searchField.toLowerCase())
         );
@@ -27,7 +34,7 @@ class App extends Component {
             return (
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundary>
                             <CardList robots={filteredRobots} />
@@ -47,4 +54,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
